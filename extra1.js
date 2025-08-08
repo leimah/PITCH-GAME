6dc197d345c89037de6689af859022111e108b69
leimah <63830567+leimah@users.noreply.github.com>
2025-08-08 16:35:23 -0700

    Update extra1.js

--- extra1.js
+++ extra1.js
@@ -1,13 +1,13 @@
 window.extra1 = [
-  "Pitch really fast",
-"Pitch internet first",
-"Speak another language",
-"Try sell a kid",
-"Pitch from too far",
-"Pitch 5 back to back",
-"Stay Hydrated!",
-"Don’t break eye contact",
-"Pitch Cx on call"
+  "Pitch really fast | FUN",
+"Pitch internet first | FUN",
+"Speak another language | FUN",
+"Try sell a kid | FUN",
+"Pitch from too far | FUN",
+"Pitch 5 back to back | FUN",
+"Stay Hydrated dear Cx! | FUN",
+"Don’t break eye contact | FUN",
+"Pitch Cx on call | FUN"
 ];
 
 //THE FUNNY
