74aa70fa6cffbce1cf913c257880ef58baa5d1e2
leimah <63830567+leimah@users.noreply.github.com>
2025-08-08 15:32:08 -0700

    Create extra3.js

---
+++ extra3.js
@@ -0,0 +1,13 @@
+window.extra3 = [
+  "4/5 LINER GOAT",
+"Translate Assist-ed",
+"Information Stand",
+"You've been DISMISSED",
+"YES YES YES",
+"FAMILY GUY",
+"Actually, get me a phone",
+"FINANCIALLY BROKEN",
+"THANKS TPV, THANKS"
+];
+
+//ACHIEVE
