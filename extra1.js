69f80e376690db252368eb671174c75f924aadc2
leimah <63830567+leimah@users.noreply.github.com>
2025-08-08 15:30:42 -0700

    Create extra1.js

---
+++ extra1.js
@@ -0,0 +1,13 @@
+window.extra1 = [
+  "Pitch really fast",
+"Pitch internet first",
+"Speak another language",
+"Try sell a kid",
+"Pitch from too far",
+"Pitch 5 back to back",
+"Stay Hydrated!",
+"Don’t break eye contact",
+"Pitch while on  call"
+];
+
+//THE FUNNY
