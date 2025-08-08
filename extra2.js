b49e040962120693f59537d2bd82fbe39eae6514
leimah <63830567+leimah@users.noreply.github.com>
2025-08-08 15:31:28 -0700

    Create extra2.js

---
+++ extra2.js
@@ -0,0 +1,13 @@
+window.extra2 = [
+  "Will ask partner",
+"Will think about it",
+"I don’t know",
+"IGNORED",
+"No English",
+"The runner",
+"No SSN/ITIN",
+"No payment method",
+"$$$ Down payment",
+];
+
+//UNCONTROL
