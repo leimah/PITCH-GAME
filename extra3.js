b2059a784b032e2a0715121c1b14fb3143e031e1
leimah <63830567+leimah@users.noreply.github.com>
2025-08-09 23:02:23 -0700

    Update extra3.js

--- extra3.js
+++ extra3.js
@@ -1,13 +1,17 @@
 window.extra3 = [
-  "4/5 LINER GOAT | 🏆",
-"Translate Assist-ed | 🏆",
-"Information Stand | 🏆",
-"You've been DISMISSED | 🏆",
-"YES YES YES | 🏆",
-"FAMILY GUY | 🏆",
-"Actually, get me a phone | 🏆",
-"FINANCIALLY BROKEN | 🏆",
-"THANKS TPV, THANKS | 🏆"
+  "4/5 LINER GOAT: Get a 4 or 5 liner | 🏆",
+"Translate Assist-ed: Assist or get assisted on a translation | 🏆",
+"Information Stand: Just give information without a sale | 🏆",
+"You've been DISMISSED: Dismiss a Time Waster | 🏆",
+"YES YES YES: Get your third YES | 🏆",
+"FAMILY GUY: sign up a whole family | 🏆",
+"Actually, get me a phone: Cx swaps BYOD for Installment | 🏆",
+"FINANCIALLY BROKEN: Cx missing payment method | 🏆",
+"THANKS TPV, THANKS: Get rejected by TPV | 🏆"
+"SOFT TO HARD: Get a credit verification | 🏆"
+"THE MAN HIMSELF: Get a visit from Ray | 🏆"
+"FIBER? FIBER? AGAIN?: Just sell fiber for a day | 🏆"
+"HIM-ENEZ: Beat Kevin in sales | 🏆"
 ];
 
 //ACHIEVE
