7f69802d635fc3e9032e1908126e641fa5a57069
leimah <63830567+leimah@users.noreply.github.com>
2025-08-17 09:58:36 -0500

    Update and rename shop.js to LIST.js
    Version 2

---
+++ LIST.js
@@ -0,0 +1,25 @@
+// LIST.js
+// --- Static events list (name + type) ---
+// type: 1 = +1 loop, 2 = +3 loops, 3 = +5 loops
+
+// list.js
+const initialEvents = [
+  { name: "will ask aartner", type: 1, icon: "🫃", completed: false },
+  { name: "will think about it", type: 1, icon: "🎲", completed: false },
+  { name: "NO SSN", type: 3, icon: "💢", completed: false },
+  { name: "Pro Time Waster", type: 2, icon: "⏰", completed: false },
+  { name: "score 1000 points", type: 1, icon: "👀", completed: false },
+  { name: "Score 3000 Points", type: 2, icon: "🥵", completed: false },
+  { name: "SCORE 10000 POINTS", type: 3, icon: "🔥", completed: false },
+  { name: "GET A SALE", type: 3, icon: "💵", completed: false },
+  { name: "No Time Right Now", type: 2, icon: "🚫", completed: false },
+  { name: "My [Family] Pays For It", type: 3, icon: "🏦", completed: false },
+  { name: "TRANSLATION ASSIST-ED", type: 3, icon: "💬", completed: false },
+  { name: "GET TPV REJECTED", type: 3, icon: "📵", completed: false },
+  { name: "CREDIT CHECK", type: 3, icon: "💳", completed: false },
+  { name: "BEAT THE HIM-ENEZ", type: 3, icon: "🏆", completed: false },
+  { name: "GET RAY'D", type: 3, icon: "🪖", completed: false },
+  { name: "4/5 LINER GOAT", type: 3, icon: "👑", completed: false },
+  { name: "Sign Up a Whole Family", type: 3, icon: "💢", completed: false },
+  // add more here
+];
